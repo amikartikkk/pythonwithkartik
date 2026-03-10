c251f57d24c3d467a7820b08c80971809884241d
amikartikkk <wkartik2006@gmail.com>
2026-03-10 16:49:55 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,3 @@
+# print numbers 1 to 30 
+for i in range(1,31):
+    print(i,end="")
---
+++ positivenegative.py
@@ -0,0 +1,10 @@
+# to check number is + or -
+#sum of 1st N natural numbers N=10
+
+number=5
+if number>0:
+    print("positive num")
+elif number<0:
+    print("Negative num")
+else:
+    print("number is zero")
