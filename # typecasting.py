aeef35c7a98e2a3ed5e9dd63f3cb770d3d489bfc
amikartikkk <wkartik2006@gmail.com>
2026-03-10 15:43:04 +0530

    Add files via upload

---
+++ # typecasting.py
@@ -0,0 +1,7 @@
+# typecasting
+a="10"
+print(type(a))
+b="20"
+print(type(b))
+c=int(a)+int(b)
+print("Addition=",c)
---
+++ # user input.py
@@ -0,0 +1,9 @@
+# user input
+# a=input("enter any number:")
+# b=input("enter any number:")
+a=int(input("enter value for a:"))
+b=int(input("enter value for b:"))
+print(type(a))
+# c=int(a)+int(b)
+c=a+b
+print("Addition =",c)
